d38221bfbc3606f1924ceebf45b4f48a4a0f3d00
Wahyu Budi <wahyubudies1@gmail.com>
2022-09-16 19:57:37 +0700

    initial local commit

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{html,js}',
+    './public/**/*.{html,js}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
