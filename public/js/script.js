3b6b4899e7fdbcd2583e8a7a28c882b187df5c86
mardhamardiya.18 <mardhamardiya.18@gmail.com>
2022-09-21 00:57:45 +0800

    Menyelesaikan Service Section

--- public/js/script.js
+++ public/js/script.js
@@ -1,7 +1,28 @@
-$(document).ready(function(){
-    $(".img-icon").mouseover(function () {
-        $(this).attr('src', $(this).data("hover"));
-      }).mouseout(function () {
-        $(this).attr('src', $(this).data("src"));
-      });
-})
+
+
+document.addEventListener( 'DOMContentLoaded', function() {
+  new Splide("#service-pertama", {
+    type    : 'loop',
+    focus: "center",
+    width: 640,
+    drag: 'free',
+  }).mount();
+
+  new Splide("#service-kedua", {
+    type    : 'loop',
+    focus: "center",
+     width: 640,
+    drag: 'free',
+  }).mount();
+
+  new Splide("#service-ketiga", {
+    perPage: 1,
+    type    : 'loop',
+    focus: "center",
+     width: 640,
+    drag: 'free',
+  }).mount();
+
+ 
+  
+});
